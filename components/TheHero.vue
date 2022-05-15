ea5523bd551120f93fd21be3e0222d9d42772635
vubinhduong <duong261100@gmail.com>
2022-05-16 01:06:10 +0700

    init

---
+++ components/TheHero.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="hero">
+      <img src="../assets/images/movieHero.jpg" alt="">
+      <div class="text-container">
+          <div class="text">
+              <span class="mini-heading">Now Streaming</span>
+              <h1><span>Now</span> Streaming</h1>
+              <a href="#movie-grid" class="button">View Movies</a>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TheHero'
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  height: 400px;
+  width: 100%;
+  position: relative;
+
+  @media (min-width: 750px) {
+    height: 500px;
+  }
+
+  &::after {
+    content: '';
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  .text-container {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    z-index: 100;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .text {
+      padding: 0 16px;
+      width: 100%;
+      max-width: 1400px;
+      margin: 0 auto;
+
+      .mini-heading {
+        font-weight: 600;
+        color: #c92502;
+        font-size: 18px;
+        text-transform: uppercase;
+        margin-bottom: 8px;
+
+        @media (min-width: 750px) {
+          font-size: 22px;
+        }
+      }
+
+      h1 {
+        color: #fff;
+        font-size: 64px;
+        font-weight: 200;
+        margin-bottom: 8px;
+        
+        @media (min-width: 750px) {
+          font-size: 84px;
+        }
+
+        span {
+          font-weight: 500;
+        }
+      }
+      
+      .button {
+        font-size: 20px;
+      }
+    }
+  }
+}
+</style>
